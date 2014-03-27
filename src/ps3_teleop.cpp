3b4a233298e3eb906cb58954365f87ea70260b17
spacemaster08 <spacemaster08@aut-065.(none)>
2014-03-27 11:19:10 +0200

    Car-like teleoperation enabled.

--- src/ps3_teleop.cpp
+++ src/ps3_teleop.cpp
@@ -43,10 +43,10 @@
 #define PS3_AXIS_ACCELEROMETER_UP        18
 #define PS3_AXIS_GYRO_YAW                19
 
-class TeleopPS3
+class TeleopPS3Car
 {
 public:
-    TeleopPS3();
+    TeleopPS3Car();
 
 private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
@@ -58,35 +58,86 @@ private:
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
     ros::ServiceClient disable, enable;
-    bool run;
+    bool run, car_like;
+    double turning_radius;
 
 };
 
 
-TeleopPS3::TeleopPS3()
+TeleopPS3Car::TeleopPS3Car()
 {
-   run = true;
+    // Default settings
+    run = true;
+    car_like = false;
+    turning_radius = 1;
+
     nh_.param("axis_linear", linear_, linear_);
     nh_.param("axis_angular", angular_, angular_);
     nh_.param("scale_angular", a_scale_, a_scale_);
     nh_.param("scale_linear", l_scale_, l_scale_);
-
+    nh_.param("car_like", car_like, car_like);
+    nh_.param("turning_radius", turning_radius, turning_radius);
 
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/ps3/cmd_vel", 1);
-    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopPS3::joyCallback, this);
+    joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopPS3Car::joyCallback, this);
 
     disable = nh_.serviceClient<std_srvs::Empty>("/ps3/disable_motors");
     enable = nh_.serviceClient<std_srvs::Empty>("/ps3/enable_motors");
 
 }
 
-void TeleopPS3::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+void TeleopPS3Car::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
     geometry_msgs::Twist vel;
 
-    vel.angular.z = a_scale_*joy->axes[angular_];
-    vel.linear.x = l_scale_*joy->axes[linear_];
-    vel_pub_.publish(vel);
+    // Car-like steering
+    if(car_like){
+
+        vel.linear.x = -l_scale_*joy->axes[PS3_AXIS_BUTTON_REAR_RIGHT_2];
+        // Backwards?
+        if(joy->buttons[PS3_BUTTON_ACTION_SQUARE]){
+            vel.linear.x *= -1;
+        }
+        // Do we have a velocity?
+        if(fabs(vel.linear.x) > 0){
+            vel.angular.z = a_scale_*joy->axes[PS3_AXIS_STICK_LEFT_LEFTWARDS];
+            // Backwards?
+            if(joy->buttons[PS3_BUTTON_ACTION_SQUARE]){
+                vel.angular.z *= -1;
+            }
+        }
+
+        // Do we have a vaild turning radius
+        if(vel.angular.z/vel.linear.x > turning_radius){
+            vel.angular.z = turning_radius * vel.linear.x;
+        } else if (vel.angular.z/vel.linear.x < -turning_radius){
+            vel.angular.z = -turning_radius * vel.linear.x;
+        }
+
+        if(joy->buttons[PS3_BUTTON_CROSS_UP]){
+            ROS_INFO("Differential Drive Mode");
+            car_like = false;
+        } else {
+            vel_pub_.publish(vel);
+        }
+
+    }
+    // Differential drive steering
+      else {
+        vel.linear.x = l_scale_*joy->axes[linear_];
+        vel.angular.z = a_scale_*joy->axes[angular_];
+        if(vel.linear.x < 0){
+            vel.angular.z *= -1;
+        }
+
+        if(joy->buttons[PS3_BUTTON_CROSS_DOWN]){
+            ROS_INFO("Car-like Drive Mode");
+            car_like = true;
+        } else {
+            vel_pub_.publish(vel);
+        }
+    }
+
 
     if(joy->buttons[PS3_BUTTON_ACTION_CROSS] && run){
         run = !run;
@@ -107,7 +158,7 @@ void TeleopPS3::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "ps3_teleop");
-    TeleopPS3 teleop_PS3;
+    TeleopPS3Car teleop_PS3;
 
     ros::spin();
 }
